34ea40960d6dab16e8473f6b43d2c7814ada98aa
Iesus Davila <iesusdavila@gmail.com>
2023-10-20 05:32:57 -0500

    Mejora en el launch para que ejecute rviz2 y gazebo a la vez

--- rov_description/launch/rov_rviz.launch.py
+++ rov_description/launch/rov_rviz.launch.py
@@ -4,13 +4,16 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 import xacro
 
 
 def generate_launch_description():
+
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
@@ -19,6 +22,13 @@ def generate_launch_description():
     pkg_path = os.path.join(get_package_share_directory('rov_description'))
     xacro_file = os.path.join(pkg_path,'urdf','loco.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
+
+    # Config time simulation
+    config_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use sim time if true'
+    )
     
     # Create a joint_state_publisher node
     node_joint_state_publisher = Node(
@@ -36,13 +46,38 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    # Gazebo Sim
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '-r empty.sdf'}.items(),
+    )
+
+    # RViz
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(pkg_path, 'rviz', 'urdf.rviz')],
+    )
+
+    # Spawn
+    spawn = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'rrbot',
+            '-topic', 'robot_description',
+        ],
+        output='screen',
+    )
+
     # Launch!
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
-
+        config_time,
         node_robot_state_publisher,
-        node_joint_state_publisher
+        node_joint_state_publisher,
+        gazebo,
+        rviz,
+        spawn
     ])
