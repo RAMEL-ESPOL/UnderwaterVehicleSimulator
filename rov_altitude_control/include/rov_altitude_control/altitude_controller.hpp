6ad7e90b255d7c6b1dd7ccc56fd6f3628bc58a59
Iesus Davila <iesusdavila@gmail.com>
2023-12-02 14:43:58 -0500

    PID ajustado para desplazamiento vertical

--- rov_altitude_control/include/rov_altitude_control/altitude_controller.hpp
+++ rov_altitude_control/include/rov_altitude_control/altitude_controller.hpp
@@ -21,9 +21,13 @@ private:
     double error_, prev_error_, integral_, derivative_;
     double kp_, ki_, kd_; // PID parameters
     double calculated_thrust_;
+    double MAX_THRUST_ = 2.5;
+    double MIN_THRUST_ = -2.5;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr pub_target_altitud_;
     std::array<rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr, 3> propulsor_publishers_;
     void initializePublishers();
     void computePID();
+    void limitValue(double& value, bool is_center=false);
     void publishThrustCommands();
 };
 
--- rov_altitude_control/src/altitude_controller.cpp
+++ rov_altitude_control/src/altitude_controller.cpp
@@ -5,7 +5,7 @@ namespace rov_altitude_control {
 AltitudeController::AltitudeController(rclcpp::Node::SharedPtr node)
     : node_(node), target_altitude_(0.0), current_altitude_(0.0),
       error_(0.0), prev_error_(0.0), integral_(0.0), derivative_(0.0),
-      kp_(2.4), ki_(0.007), kd_(0.02), calculated_thrust_(0.0) {
+      kp_(150.0), ki_(1.0), kd_(140.0), calculated_thrust_(0.0) {
     initializePublishers();
 }
 
@@ -19,6 +19,11 @@ void AltitudeController::updateControl(const nav_msgs::msg::Odometry::SharedPtr&
     integral_ += error_;
     derivative_ = error_ - prev_error_;
     prev_error_ = error_;
+
+    std_msgs::msg::Float64 target_altitud_msg;
+    target_altitud_msg.data = target_altitude_;
+    pub_target_altitud_->publish(target_altitud_msg);
+
     computePID();
     publishThrustCommands();
 }
@@ -29,12 +34,30 @@ void AltitudeController::initializePublishers() {
     propulsor_publishers_[2] = node_->create_publisher<std_msgs::msg::Float64>("/model/rov_max/joint/shell_to_center_thrust/cmd_thrust", 10);
     /*propulsor_publishers_[3] = node_->create_publisher<std_msgs::msg::Float64>("/model/rov_max/joint/shell_to_right_thrust/cmd_thrust", 10);
     propulsor_publishers_[4] = node_->create_publisher<std_msgs::msg::Float64>("/model/rov_max/joint/shell_to_left_thrust/cmd_thrust", 10);*/
+
+    pub_target_altitud_ = node_->create_publisher<std_msgs::msg::Float64>("/model/rov_max/longitud_controller/target_altitud", 10);
 }
 
 void AltitudeController::computePID() {
     double thrust = kp_ * error_ + ki_ * integral_ + kd_ * derivative_;
     // Normalizar o limitar el valor de 'thrust' si es necesario
     calculated_thrust_ = thrust;
+}
+
+void AltitudeController::limitValue(double& value, bool is_center) {
+    if (is_center) {
+        if (value < MIN_THRUST_*1.25) {
+            value = MIN_THRUST_*1.25;
+        } else if (value > MAX_THRUST_*1.25) {
+            value = MAX_THRUST_*1.25;
+        }
+    } else {
+        if (value < MIN_THRUST_) {
+            value = MIN_THRUST_;
+        } else if (value > MAX_THRUST_) {
+            value = MAX_THRUST_;
+        }
+    }
 }
 
 void AltitudeController::publishThrustCommands() {
@@ -44,6 +67,9 @@ void AltitudeController::publishThrustCommands() {
     for (auto& publisher : propulsor_publishers_) {
         if (publisher == propulsor_publishers_[2]) {
             thrust_msg.data = thrust_msg.data * 1.23;
+            limitValue(thrust_msg.data, true);
+        }else{
+            limitValue(thrust_msg.data);
         }
         RCLCPP_INFO(node_->get_logger(), "Thrust: %f", thrust_msg.data);
         publisher->publish(thrust_msg);
