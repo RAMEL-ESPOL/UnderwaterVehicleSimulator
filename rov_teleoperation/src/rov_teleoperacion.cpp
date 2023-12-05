e3a38b41844975665b3daf3de15965fa77e4abea
Iesus Davila <iesusdavila@gmail.com>
2023-12-05 15:40:47 -0500

    Teleoperacion para control de posicion angular

--- rov_teleoperation/src/rov_teleoperacion.cpp
+++ rov_teleoperation/src/rov_teleoperacion.cpp
@@ -1,6 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
-#include "rov_teleoperation/control_profundidad.hpp"
-#include "rov_teleoperation/control_posxy.hpp"
+// #include "rov_teleoperation/control_profundidad.hpp"
+// #include "rov_teleoperation/control_posxy.hpp"
+#include "rov_teleoperation/control_yaw.hpp" 
+#include "rov_teleoperation/control_pitch.hpp"
 #include <nav_msgs/msg/odometry.hpp>
 //#include <std_srvs/srv/set_float64.hpp>
 #include <ncurses.h>
@@ -20,9 +22,13 @@ public:
 
         double targetProfundidad = -0.1;
         double targetPosXY = -5.0;
+        double targetYaw = 0.0;
+        double targetPitch = 0.0;
 
-        control_profundidad_->setTargetProfundidad(targetProfundidad);
-        control_posXY_->setTargetPosXY(targetPosXY);
+        //control_profundidad_->setTargetProfundidad(targetProfundidad);
+        //control_posXY_->setTargetPosXY(targetPosXY);
+        control_yaw_->setTargetYaw(targetYaw);
+        control_pitch_->setTargetPitch(targetPitch);
 
         int ch;
         while (rclcpp::ok()) {
@@ -41,25 +47,43 @@ public:
                 case KEY_RIGHT:
                     targetPosXY -= 0.1;
                     break;
+                case 'a':
+                    targetYaw += 0.1;
+                    break;
+                case 'd':
+                    targetYaw -= 0.1;
+                    break;
+                case 'w':
+                    targetPitch -= 0.1;
+                    break;
+                case 's':
+                    targetPitch += 0.1;
+                    break;
                 case 'q':
                     endwin(); // Finalizar ncurses antes de salir
                     return; // Sale del bucle y del hilo
             }
-            control_posXY_->setTargetPosXY(targetPosXY);
-            control_profundidad_->setTargetProfundidad(targetProfundidad);
+            //control_posXY_->setTargetPosXY(targetPosXY);
+            //control_profundidad_->setTargetProfundidad(targetProfundidad);
+            control_yaw_->setTargetYaw(targetYaw);
+            control_pitch_->setTargetPitch(targetPitch);
         }
         endwin();
     }
 
     void init() {
-        control_profundidad_ = std::make_shared<rov_control_profundidad::ControlProfundidad>(shared_from_this());
-        control_posXY_ = std::make_shared<rov_control_posxy::ControlPosXY>(shared_from_this());
+        //control_profundidad_ = std::make_shared<rov_control_profundidad::ControlProfundidad>(shared_from_this());
+        //control_posXY_ = std::make_shared<rov_control_posxy::ControlPosXY>(shared_from_this());
+        control_yaw_ = std::make_shared<rov_control_yaw::ControlYaw>(shared_from_this());
+        control_pitch_ = std::make_shared<rov_control_pitch::ControlPitch>(shared_from_this());
 
         odometry_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
             "/model/rov_max/odometry", 10,
             [this](const nav_msgs::msg::Odometry::SharedPtr msg) {
-                control_profundidad_->updateControl(msg);
-                control_posXY_->updateControl(msg);
+                //control_profundidad_->updateControl(msg);
+                //control_posXY_->updateControl(msg);
+                control_yaw_->updateControl(msg);
+                control_pitch_->updateControl(msg);
             });
 
         // Crear un hilo para la teleoperación
@@ -69,8 +93,10 @@ public:
 
 private:
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odometry_subscription_;
-    std::shared_ptr<rov_control_profundidad::ControlProfundidad> control_profundidad_;
-    std::shared_ptr<rov_control_posxy::ControlPosXY> control_posXY_;
+    //std::shared_ptr<rov_control_profundidad::ControlProfundidad> control_profundidad_;
+    //std::shared_ptr<rov_control_posxy::ControlPosXY> control_posXY_;
+    std::shared_ptr<rov_control_yaw::ControlYaw> control_yaw_;
+    std::shared_ptr<rov_control_pitch::ControlPitch> control_pitch_;
 };
 
 int main(int argc, char** argv) {
