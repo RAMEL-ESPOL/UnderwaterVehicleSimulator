d08cf97eb6178c620eb5ac74eaa5cafd8dc30476
Iesus Davila <iesusdavila@gmail.com>
2023-12-29 08:35:27 -0500

    Remapear el topic del Pose por problemas constantes con el frame_id

--- auv_max_node/include/auv_max_node/gazebo_rviz_synchronizer.hpp
+++ auv_max_node/include/auv_max_node/gazebo_rviz_synchronizer.hpp
@@ -1,6 +1,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <tf2_ros/transform_broadcaster.h>
 #include <nav_msgs/msg/odometry.hpp>
+#include <geometry_msgs/msg/pose_array.hpp>
 
 class OdometryToTFPublisher : public rclcpp::Node {
 public:
@@ -10,4 +11,7 @@ private:
     void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
     std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+
+    rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr sub_pose_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr pub_pose_;
 };
--- auv_max_node/src/gazebo_rviz_synchronizer.cpp
+++ auv_max_node/src/gazebo_rviz_synchronizer.cpp
@@ -6,6 +6,23 @@ OdometryToTFPublisher::OdometryToTFPublisher() : Node("odometry_to_tf_publisher"
         std::bind(&OdometryToTFPublisher::odomCallback, this, std::placeholders::_1));
 
     tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(*this);
+
+    sub_pose_ = this->create_subscription<geometry_msgs::msg::PoseArray>(
+        "/model/auv_max/pose",
+        10,
+        [this](const geometry_msgs::msg::PoseArray::SharedPtr msg) {
+            auto new_msg = *msg;
+            new_msg.header.stamp = this->now();
+            new_msg.header.frame_id = "auv_max_shell"; // Nuevo frame_id
+            RCLCPP_INFO(this->get_logger(), "PoseArray received and remapped");
+            pub_pose_->publish(new_msg);
+        }
+    );
+
+    pub_pose_ = this->create_publisher<geometry_msgs::msg::PoseArray>(
+        "/model/auv_max/remapped_pose",
+        10
+    );
 }
 
 void OdometryToTFPublisher::odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
