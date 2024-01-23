8ceb3b92991fb6e0e4383773217e051ecd034a99
Iesus Davila <iesusdavila@gmail.com>
2024-01-23 16:11:35 -0500

    Cambio de sonar 2D a sonar 3D

---
+++ auv_max_cartographer/launch/cartographer.launch.py
@@ -0,0 +1,47 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    auv_max_cartographer_prefix = get_package_share_directory('auv_max_cartographer')
+    cartographer_config_dir = LaunchConfiguration('cartographer_config_dir', default=os.path.join(
+                                                  auv_max_cartographer_prefix, 'config'))
+    configuration_basename = LaunchConfiguration('configuration_basename',
+                                                 default='cartographer_config.lua')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'cartographer_config_dir',
+            default_value=cartographer_config_dir,
+            description='Full path to the config file to load'),
+        DeclareLaunchArgument(
+            'configuration_basename',
+            default_value=configuration_basename,
+            description='Name of the lua file for Cartographer'),
+
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_node',
+            name='cartographer_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-configuration_directory', cartographer_config_dir,
+                       '-configuration_basename', configuration_basename],
+            remappings=[('/imu', '/model/auv_max/imu'),
+                        ('/odom', '/model/auv_max/odometry'),
+                        ('/horizontal_laser_3d', '/model/auv_max/remapped_sonar_point_cloud')]),
+        
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_occupancy_grid_node',
+            name='cartographer_occupancy_grid_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-resolution', '0.05', '-publish_period_sec', '1.0']),
+    ])
